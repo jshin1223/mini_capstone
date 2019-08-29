c74d225ddb688718c9790ea03341abf45f6db30c
jshin1223 <jshin1223@gmail.com>
2019-08-29 11:59:32 -0500

    all_products_route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductsController < ApplicationController
     @product = Product.first
     render 'first_product_view.json.jb'
   end
+
+  def all_products
+    @products = Product.all
+    render 'all_products.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/first_product_url' => 'products#first_product_action'
+    get '/all_products' => 'products#all_products'
   end 
 end 
 
